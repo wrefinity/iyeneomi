e76e32390ee0bc59f5afa07ba1a39cc621289b57
wrefinity <wrefordmessi@gmail.com>
2025-07-02 20:32:07 +0100

    image addons

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -250,7 +250,7 @@ export default function HeroSection() {
               <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black opacity-30 z-10" />
               
               <img
-                src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80"
+                src="/iye.jpg"
                 alt="Iyeneomi Blessing Ogoina"
                 className="w-full h-full object-cover"
               />
